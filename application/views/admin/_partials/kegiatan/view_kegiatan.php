3c5e7705206890da0e7cecb1fdfb2752bc9b99d3
Dhavina Ocxa <dhavinaoxca123@gmail.com>
2024-07-09 08:31:05 +0700

    commit tim emak

--- application/views/admin/_partials/kegiatan/view_kegiatan.php
+++ application/views/admin/_partials/kegiatan/view_kegiatan.php
@@ -1,6 +1,13 @@
 <div class="row">
     <div class="col-md-12 grid-margin stretch-card">
         <div class="card">
+            <div class="card-title">
+                <div style="margin: 20px;">
+                    <a href="<?= base_url("admin/kegiatan/plot_kegiatan") ?>">
+                        <button class="btn btn-primary btn-icon-text mb-md-0">Tambah Data</button>
+                    </a>
+                </div>
+            </div>
             <div class="card-body">
                 <h6 class="card-title">Data Kegiatan di Kota Surabaya</h6>
                 <p class="text-muted mb-3">Data Kegiatan di Pemerintah Kota Surabaya</p>
@@ -15,7 +22,7 @@
                                 <th width="20px">Giat</th>
                                 <th width="20px">Waktu Kegiatan</th>
                                 <th width="30px">Kegiatan</th>
-                                <th width="20px">Lokasi Kejadian</th>
+                                <th width="20px">Lokasi Kegiatan</th>
                                 <th width="20px">Jumlah Personel</th>
                                 <th width="30px">Jumlah Jarko</th>
                                 <th width="30px">Keterangan</th>
