427212107deaf7f9d9d9a47d9e6bb48ac1314ab1
Rizukaf-id <rizukafid@gmail.com>
2024-07-19 15:02:43 +0700

    dropdwon petugas di lokasi penemuan jenazah dan pohon tumbang selesai

--- application/views/admin/data_kejadian/new_form_pohon_tumbang.php
+++ application/views/admin/data_kejadian/new_form_pohon_tumbang.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
 	<?php $this->load->view("admin/_partials/head") ?>
 </head>
 <body>
@@ -18,6 +19,7 @@
 	</div>
 
 	<?php $this->load->view("admin/_partials/js") ?>
-
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 </body>
 </html>
