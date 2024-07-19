c6e2b7e81a71ade536a783572e0c0a7734bef9ab
ismiazzah <ismiazzahk4@gmail.com>
2024-07-19 10:09:10 +0700

    update tampilan admin

--- application/views/admin/_partials/data_kejadian/add.php
+++ application/views/admin/_partials/data_kejadian/add.php
@@ -1,4 +1,3 @@
-
 <div class="row">
   <div class="col-md-12 grid-margin stretch-card">
     <div class="card">
@@ -352,3 +351,18 @@ if (window.addEventListener) {
 });
 
 </script>
+<style>
+  .alert-container {
+  position: fixed;
+  top: 15%;
+  left: 57%;
+  transform: translate(-50%, -50%);
+  width: auto;
+  z-index: 1050;
+}
+
+#success-alert {
+  margin-bottom: 0; /* Remove bottom margin to prevent extra space */
+}
+
+  </style>
